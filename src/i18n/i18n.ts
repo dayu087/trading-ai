7abafbf7e1889053a3eea27f0a91376c986067c7
dayu <zhoushank@foxmail.com>
2025-11-24 14:40:45 +0800

    --

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,26 +1,32 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './en.json'
 import zh from './zh.json'
 
 i18n
-  .use(initReactI18next) // 连接 React
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       zh: { translation: zh },
     },
-    lng: 'en',
+
     fallbackLng: 'en',
+
+    load: 'currentOnly',
+
     supportedLngs: ['en', 'zh'],
-    debug: true,
+
+    detection: {
+      order: ['localStorage', 'cookie'],
+      caches: ['localStorage', 'cookie'],
+      lookupLocalStorage: 'i18nextLng',
+    },
+
     interpolation: {
       escapeValue: false,
     },
-    backend: {
-      loadPath: 'src/i18n/{{ns}}.json',
-    },
   })
-
-export default i18n
