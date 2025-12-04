806624029399c41e4a8468b3227181da904faa04
dayu <zhoushank@foxmail.com>
2025-12-04 23:16:13 +0800

    --

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Toaster } from 'sonner'
 import './index.css'
 import './i18n/i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster
+      theme="light"
+      richColors
+      closeButton
+      position="top-center"
+      duration={2200}
+      toastOptions={{
+        className: 'nofx-toast',
+        style: {
+          background: '#fff',
+          border: '1px solid var(--border-black)',
+          color: 'var(--brand-black)',
+        },
+      }}
+    />
     <App />
   </React.StrictMode>
 )
